daff9961601fdd436e689e837ec4e727d4a651eb
Guilherme Liborio Machado <liborio.ofc@gmail.com>
2023-02-05 14:48:17 -0300

    criando a configuração padrão para inciar a aplicação

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const app = express();
 app.get("/",(req,res)=>{
     res.send("");
 });
+
+app.listen(8080,(erro) =>{
+    if(!erro){
+        console.log("O servidor está rodando!")
+    } else {
+        console.log(erro)
+    }
+});
