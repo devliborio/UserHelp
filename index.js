1950a872511a834d74f1ba5054a978acf5b60601
Guilherme Liborio Machado <liborio.ofc@gmail.com>
2023-02-05 16:23:22 -0300

    importando variavel de conexão ao banco dados

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/connection");
 
 // View engine
 app.set("view engine", "ejs");
