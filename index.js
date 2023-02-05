bf6860675ce5f4c1c9074994c7060ec40cf581dc
Guilherme Liborio Machado <liborio.ofc@gmail.com>
2023-02-05 16:28:00 -0300

    Criando autenticação ao banco de dados atraves do dados fornecidos no arquivo conection.js

--- index.js
+++ index.js
@@ -10,18 +10,28 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 // Body parser
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Database
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feito com o banco de dados!");
+    }).catch((error) => {
+        console.log(error);
+    });
 
 // Rotas
 app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.listen(8080, (erro) => {
-    if (!erro) {
+app.listen(8080, (error) => {
+    if (!error) {
         console.log("O servidor está rodando!")
     } else {
-        console.log(erro)
+        console.log(error);
     }
 });
