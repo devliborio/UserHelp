0c62530bbc0bd269cd2d2b4dfe534a465ec78c39
Guilherme Liborio Machado <liborio.ofc@gmail.com>
2023-02-05 16:14:04 -0300

    configurando expressjs para receber arquivos estáticos

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const bodyParser = require("body-parser");
 // View engine
 app.set("view engine", "ejs");
 
+// Static
+app.use(express.static("public"));
+
 // Body parser
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
