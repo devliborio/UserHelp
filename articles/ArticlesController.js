6d1ca1bfe98e38c4a34be33cc8c47036b43aba40
Guilherme Liborio Machado <liborio.ofc@gmail.com>
2023-02-06 12:08:52 -0300

    Nova rota para criar articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-
 router.get("/articles", (req,res) => {
     res.send("");
 });
 
+router.get("/admin/articles/new",(req,res) => {
+    res.send("");
+});
 
 module.exports = router;
