c176d8a63b9d716bc8068980f8ad86f271d902a5
Guilherme Liborio Machado <liborio.ofc@gmail.com>
2023-02-06 12:35:35 -0300

    Configurando model de categorias

--- categories/CategoryModel.js
+++ categories/CategoryModel.js
@@ -1,2 +1,16 @@
 const Sequelize = require("sequilize");
 const connection = require("../database/connection");
+
+const Category = connection.define('categories',{
+    title:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    
+    slug: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = Category;
