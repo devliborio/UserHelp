de71b6979b7dfad5720b4dbc24ef60082d5e0de4
Guilherme Liborio Machado <liborio.ofc@gmail.com>
2023-02-06 18:09:41 -0300

    configurando rota para renderizar EJS de novas categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -5,8 +5,8 @@ router.get("/categories", (req,res) => {
     res.send("");
 });
 
-router.get("admin/categories/new", (req,res) => {
-    res.send("");
+router.get("/admin/categories/new", (req,res) => {
+    res.render("admin/categories/new");
 });
 
 module.exports = router;
