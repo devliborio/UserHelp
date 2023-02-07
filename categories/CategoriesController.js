8226fda60bbaed05349396a508ddc3d15ffad24c
Guilherme Liborio Machado <liborio.ofc@gmail.com>
2023-02-07 13:18:59 -0300

    Configurando rota para tratar formulario de categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -7,10 +7,17 @@ router.get("/admin/categories/new", (req,res) => {
     res.render("admin/categories/new");
 });
 
-router.get("/categories/save",(req,res) => {
-    var title = req.body.title;
+router.post("/categories/save",(req,res) => {
+    var title = req.body.title; // Usando o body-parser para pegar input do formulario
     if(title != undefined){
-        
+
+        CategoryModel.create({ // Tratando dados do formulario para salvar no banco de dados
+            title: title,
+            slug: slugify(title) // Usando o slugify para transformar o titulo da categoria em um slug.
+        }).then(() => {
+            res.redirect("/");
+        });
+
     } else {
         res.redirect("/admin/categories/new");
     }
