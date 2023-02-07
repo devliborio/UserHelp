48e5fbb2ef0d9f3ee72c735d54b83eab3f93f87c
Guilherme Liborio Machado <liborio.ofc@gmail.com>
2023-02-07 12:50:24 -0300

    Removendo rota /categories

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/categories", (req,res) => {
-    res.send("");
-});
-
 router.get("/admin/categories/new", (req,res) => {
     res.render("admin/categories/new");
 });
