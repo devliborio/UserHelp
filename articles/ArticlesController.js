fc3da09ef86f7d01fbff835f8f7bafd5feec62e8
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-08 13:32:49 -0300

    Importando model do artigo e o slugify

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const CategoryModel = require("../categories/CategoryModel");
+const AricleModel = require("./ArticleModel");
+const slugify = require("slugify");
 
 router.get("/admin/articles/new", (req, res) => {
     CategoryModel.findAll().then((categories) => {
