83ccad98f233fd32d7e2390edb4436a1aacbc0a4
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-08 13:21:18 -0300

    Importando model de categorias

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const CategoryModel = require("../categories/CategoryModel");
 
 router.get("/admin/articles/new", (req, res) => {
     res.render("admin/articles/new");
