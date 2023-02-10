7df6a873b297eef57e520dc318f2e84d16306e05
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-10 16:00:39 -0300

    Implementando e configurando todo o sistema de cadastro de usuarios

--- users/UsersController.js
+++ users/UsersController.js
@@ -9,7 +9,10 @@ const UserModel = require("./UserModel")
 
 // Routes type get()
 router.get("/admin/users", (req, res) => {
-    res.send("");
+    UserModel.findAll().then((users) => {
+        res.render("admin/users/index", { users: users });
+
+    });
 });
 
 router.get("/admin/users/create", (req, res) => {
@@ -17,9 +20,42 @@ router.get("/admin/users/create", (req, res) => {
 });
 
 // Routes type post()
-router.post("/users/create", (req, res) => {
+router.post("/users/create", (req, res) => { // Salvando dados do usuario no banco de dados (mysql)
     var email = req.body.email;
     var password = req.body.password;
+
+    UserModel.findOne({ where: { email: email } }).then((user) => {
+
+        if (user == undefined) { // Se não achar está permitido criar com o email solicitado
+
+            // Etapa de segurança para armazenar a senha do usuario em conversão hash
+            var salt = bcrypt.genSaltSync(10); // gerando incremento pro hash
+            var hash = bcrypt.hashSync(password, salt); // ele vai retornar o hash pra variavel
+
+            if (email && password != undefined) {
+                UserModel.create({
+
+                    email: email,
+                    password: hash
+
+                }).then(() => {
+                    res.redirect("/");
+
+                }).catch(err => {
+                    console.log(err);
+
+                });
+
+            } else {
+                res.send("Preencha todos os campos!")
+            }
+
+
+        } else { // Se achou algum email igual
+            res.send("Email digitado já foi cadastrado, tente novamente")
+
+        }
+    });
 
     // res.json({ email, password }); // Testando pra conferir se está enviando os dados
 });
