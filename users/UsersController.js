cc6a049eba10d093d1f1ba75a4821d94589539d0
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-10 13:23:55 -0300

    Nova rota para receber os dados cadastrados pelo usuario

--- users/UsersController.js
+++ users/UsersController.js
@@ -5,12 +5,20 @@ const router = express.Router();
 const UserModel = require("./UserModel")
 
 // Routes type get()
-router.get("/admin/users", (req,res) => {
+router.get("/admin/users", (req, res) => {
     res.send("");
 });
 
-router.get("/admin/users/create", (req,res) => {
+router.get("/admin/users/create", (req, res) => {
     res.render("admin/users/new");
+});
+
+// Routes type post()
+router.post("/users/create", (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    // res.json({ email, password }); // Testando pra conferir se está enviando os dados
 });
 
 module.exports = router;
