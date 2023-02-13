378c2bd25c5aa5083309ed9bda8246d7126a654f
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-13 10:10:50 -0300

    Redirecionando usuário para pagina de login caso não tenha autenticação

--- middlewares/adminAuth.js
+++ middlewares/adminAuth.js
@@ -4,7 +4,7 @@ function adminAuth(req, res, next) { // Um middleware está entre a requisição
         next(); // Se você não chamar o 3 parametro a requisição vai ficar presa!
 
     } else { // Se ele não estiver logado ele vai pra homepage se tentar acessar a rota admin!
-        res.redirect("/");
+        res.redirect("/login");
     }
 }
 
